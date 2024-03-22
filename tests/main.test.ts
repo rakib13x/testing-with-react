19e78b940ae9c6dd570a8755f121f17bd3b8d381
Rakib Islam <rakib71xx@gmail.com>
2024-03-22 23:33:31 +0600

    testing for ProductList updated

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,10 +1,9 @@
 import { it, expect, describe } from "vitest";
+import { db } from "./mocks/db";
 
 describe("group", () => {
-  it("should", async () => {
-    const response = await fetch("/categories");
-    const data = await response.json();
-    console.log(data);
-    expect(data).toHaveLength(3);
+  it("should", () => {
+    const product = db.product.create();
+    console.log(product);
   });
 });
