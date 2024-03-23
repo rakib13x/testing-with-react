5ef74485506037299d2f3c30c47dca827aa5405e
Rakib Islam <rakib71xx@gmail.com>
2024-03-24 04:06:21 +0600

    added test data rendering

--- tests/pages/BrowseProductsPage.test.tsx
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -1,19 +1,40 @@
+import { Theme } from "@radix-ui/themes";
 import {
   render,
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import { http, delay, HttpResponse } from "msw";
-import { server } from "../mocks/server";
+import userEvent from "@testing-library/user-event";
+import { HttpResponse, delay, http } from "msw";
+import { Category, Product } from "../../src/entities";
 import BrowseProducts from "../../src/pages/BrowseProductsPage";
-import { Theme } from "@radix-ui/themes";
+import { CartProvider } from "../../src/providers/CartProvider";
+import { db } from "../mocks/db";
+import { server } from "../mocks/server";
 
 describe("BrowseProductPage", () => {
+  const categories: Category[] = [];
+  const products: Product[] = [];
+  beforeAll(() => {
+    [1, 2].forEach((item) => {
+      categories.push(db.category.create({ name: "Category" + item }));
+      products.push(db.product.create());
+    });
+  });
+  afterAll(() => {
+    const categoryIds = categories.map((c) => c.id);
+    db.category.deleteMany({ where: { id: { in: categoryIds } } });
+
+    const productIds = products.map((p) => p.id);
+    db.product.deleteMany({ where: { id: { in: productIds } } });
+  });
   const renderComponent = () => {
     render(
-      <Theme>
-        <BrowseProducts />
-      </Theme>
+      <CartProvider>
+        <Theme>
+          <BrowseProducts />
+        </Theme>
+      </CartProvider>
     );
   };
   it("should show a loading skeleton when fetching categories", () => {
@@ -79,5 +100,34 @@ describe("BrowseProductPage", () => {
     renderComponent();
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
+
+  it("should render categories ", async () => {
+    renderComponent();
+
+    const combobox = await screen.findByRole("combobox");
+    expect(combobox).toBeInTheDocument();
+
+    const user = userEvent.setup();
+    await user.click(combobox);
+
+    expect(screen.getByRole("option", { name: /all/i })).toBeInTheDocument();
+    categories.forEach((category) => {
+      expect(
+        screen.getByRole("option", { name: category.name })
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("should render products", async () => {
+    renderComponent();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole("progressbar", { name: /products/i })
+    );
+
+    products.forEach((product) => {
+      expect(screen.getByText(product.name)).toBeInTheDocument();
+    });
   });
 });
