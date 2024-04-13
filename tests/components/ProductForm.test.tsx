b121697d3ba0243bfa75d9554ff7821a41fea22f
Rakib Islam <rakib71xx@gmail.com>
2024-04-13 10:01:28 +0600

    testing for name and price input field added

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -22,13 +22,52 @@ describe("ProductForm", () => {
     });
 
     return {
+      expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
+        const error = screen.getByRole("alert");
+        expect(error).toBeInTheDocument();
+        expect(error).toHaveTextContent(errorMessage);
+      },
       waitForFormToLoad: async () => {
         await screen.findByRole("form");
+
+        const nameInput = screen.getByPlaceholderText(/name/i);
+        const priceInput = screen.getByPlaceholderText(/price/i);
+        const categoryInput = screen.getByRole("combobox", {
+          name: /category/i,
+        });
+        const SubmitButton = screen.getByRole("button");
+
+        type FormData = {
+          [K in keyof Product]: any;
+        };
+
+        const validData: FormData = {
+          id: 1,
+          name: "a",
+          price: 1,
+          categoryId: 1,
+        };
+
+        const fill = async (product: FormData) => {
+          const user = userEvent.setup();
+
+          if (product.name !== undefined)
+            await user.type(nameInput, product.name);
+          if (product.price !== undefined)
+            await user.type(priceInput, product.price.toString());
+          await user.click(categoryInput);
+          const options = screen.getAllByRole("option");
+          await user.click(options[0]);
+          await user.click(SubmitButton);
+        };
+
         return {
-          nameInput: screen.getByPlaceholderText(/name/i),
-          priceInput: screen.getByPlaceholderText(/price/i),
-          categoryInput: screen.getByRole("combobox", { name: /category/i }),
-          SubmitButton: screen.getByRole("button"),
+          nameInput,
+          priceInput,
+          categoryInput,
+          SubmitButton,
+          fill,
+          validData,
         };
       },
     };
@@ -66,18 +105,62 @@ describe("ProductForm", () => {
     expect(nameInput).toHaveFocus();
   });
 
-  it("should display an error if name is missing", async () => {
-    const { waitForFormToLoad } = renderComponent();
+  it.each([
+    {
+      scenerio: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenerio: "longer than 255 characters",
+      name: "a".repeat(256),
+      errorMessage: /255/,
+    },
+  ])(
+    "should display an error if name is $scenerio",
+    async ({ name, errorMessage }) => {
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
 
-    const form = await waitForFormToLoad();
-    const user = userEvent.setup();
-    await user.type(form.priceInput, "10");
-    await user.click(form.categoryInput);
-    const options = screen.getAllByRole("option");
-    await user.click(options[0]);
-    await user.click(form.SubmitButton);
-    const error = screen.getByRole("alert");
-    expect(error).toBeInTheDocument();
-    expect(error).toHaveTextContent(/required/i);
-  });
+      const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, name });
+
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
+  it.each([
+    {
+      scenerio: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenerio: "0",
+      price: 0,
+      errorMessage: /1/,
+    },
+    {
+      scenerio: "negative",
+      price: -1,
+      errorMessage: /1/,
+    },
+    {
+      scenerio: "greater than 1000",
+      price: 1001,
+      errorMessage: /1000/,
+    },
+    {
+      scenerio: "not a number",
+      price: "a",
+      errorMessage: /required/,
+    },
+  ])(
+    "should display an error if price is $scenerio",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
+
+      const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, price });
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
 });
