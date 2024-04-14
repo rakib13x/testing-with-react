bfad93488145774dd7681960f7aed09f148a4f5b
Rakib Islam <rakib71xx@gmail.com>
2024-04-14 11:33:12 +0600

    testing for invalid routes  added

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -26,4 +26,9 @@ describe("Router", () => {
 
     db.product.delete({ where: { id: { equals: product.id } } });
   });
+
+  it("should render the not found page for invalid routes", () => {
+    navigateTo("/invalid-route");
+    expect(screen.getByText(/not found/i)).toBeInTheDocument();
+  });
 });
