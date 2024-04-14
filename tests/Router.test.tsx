e73b950ef18bc05f1626cf3eed96fc08d2ae68eb
Rakib Islam <rakib71xx@gmail.com>
2024-04-14 12:22:20 +0600

    testing for ProductDetailPage added

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -3,8 +3,9 @@ import { db } from "./mocks/db";
 import { navigateTo } from "./utils";
 
 describe("Router", () => {
-  it("should render the home page", () => {
+  it("should render the home page for /", () => {
     navigateTo("/");
+
     expect(screen.getByRole("heading", { name: /home/i })).toBeInTheDocument();
   });
 
@@ -15,10 +16,11 @@ describe("Router", () => {
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
   });
+
   it("should render the product details page for /products/:id", async () => {
     const product = db.product.create();
 
-    navigateTo("/products" + product.id);
+    navigateTo("/products/" + product.id);
 
     expect(
       await screen.findByRole("heading", { name: product.name })
@@ -29,10 +31,13 @@ describe("Router", () => {
 
   it("should render the not found page for invalid routes", () => {
     navigateTo("/invalid-route");
+
     expect(screen.getByText(/not found/i)).toBeInTheDocument();
   });
+
   it("should render the admin home page for /admin", () => {
-    navigateTo("/invalid-route");
+    navigateTo("/admin");
+
     expect(screen.getByRole("heading", { name: /admin/i })).toBeInTheDocument();
   });
 });
