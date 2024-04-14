183c32e9a5938baad49cd96be2072a5aa1d546fb
Rakib Islam <rakib71xx@gmail.com>
2024-04-14 12:23:14 +0600

    testing complete

--- tests/mocks/data.ts
+++ tests/mocks/data.ts
@@ -1,5 +1,5 @@
 export const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
-  { id: 3, name: "Product 3", price: 30 },
+  { id: 3, name: "Product 4", price: 30 },
 ];
