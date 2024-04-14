b12ea8cd4c81ea1721e9e508261759917680fcdb
Rakib Islam <rakib71xx@gmail.com>
2024-04-14 11:30:39 +0600

    testing routes with parameter added

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,26 +1,29 @@
-import { render, screen } from "@testing-library/react";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
-import routes from "../src/routes";
+import { screen } from "@testing-library/react";
+import { db } from "./mocks/db";
+import { navigateTo } from "./utils";
 
 describe("Router", () => {
   it("should render the home page", () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ["/"],
-    });
-
-    render(<RouterProvider router={router} />);
+    navigateTo("/");
     expect(screen.getByRole("heading", { name: /home/i })).toBeInTheDocument();
   });
 
   it("should render the products page for /products", () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ["/products"],
-    });
-
-    render(<RouterProvider router={router} />);
+    navigateTo("/products");
 
     expect(
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
+  });
+  it("should render the product details page for /products/:id", async () => {
+    const product = db.product.create();
+
+    navigateTo("/products" + product.id);
+
+    expect(
+      await screen.findByRole("heading", { name: product.name })
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
   });
 });
--- tests/utils.tsx
+++ tests/utils.tsx
@@ -1,5 +1,8 @@
 import { User, useAuth0 } from "@auth0/auth0-react";
+import { render } from "@testing-library/react";
 import { HttpResponse, delay, http } from "msw";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import routes from "../src/routes";
 import { server } from "./mocks/server";
 
 export const simulateDelay = (endpoint: string) => {
@@ -32,4 +35,12 @@ export const mockState = (authState: AuthState) => {
     logout: vi.fn(),
     handleRedirectCallback: vi.fn(),
   });
+};
+
+export const navigateTo = (path: string) => {
+  const router = createMemoryRouter(routes, {
+    initialEntries: [path],
+  });
+
+  render(<RouterProvider router={router} />);
 };
