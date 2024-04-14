392197893f0faf5930ddee1a3c8905cf81f4000e
Rakib Islam <rakib71xx@gmail.com>
2024-04-14 10:41:01 +0600

    testing for homePage and productsPage added

---
+++ tests/Router.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import routes from "../src/routes";
+
+describe("Router", () => {
+  it("should render the home page", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/"],
+    });
+
+    render(<RouterProvider router={router} />);
+    expect(screen.getByRole("heading", { name: /home/i })).toBeInTheDocument();
+  });
+
+  it("should render the products page for /products", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/products"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    expect(
+      screen.getByRole("heading", { name: /products/i })
+    ).toBeInTheDocument();
+  });
+});
